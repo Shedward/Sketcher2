098738c0334b4bfc93618adc41345f26c7d98208
Vlad Maltsev <maltsevvn@m2.ru>
2020-07-10 23:51:22 +0300

    Initial Commit

---
+++ Shared/Sketcher_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sketcher_SUIApp.swift
+//  Shared
+//
+//  Created by Vlad Maltsev on 10.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct Sketcher_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
